151f62bf9e412da512a88fde49d472e3f804ebf9
Carlos Felipe Aguirr <Carlos Felipe Aguirr>
2016-04-13 15:45:57 -0500

    Se ha aÃadido un manual en el index de la aplicacion ademas se ha mejorado la maquetacion de la vista de consultar tickets y rediseÃ±ado el icono de esa misma vista.

--- application/views/defecto/index.php
+++ application/views/defecto/index.php
@@ -1,7 +1,11 @@
-<h1 class="w3-center w3-text-indigo animated pulse">Mesa de ayuda</h1>
-<div class="w3-container w3-border w3-round" style="width: 85%; margin: auto">
+<div class="w3-container w3-border w3-round w3-border-indigo" style="width: 85%; margin: auto; border-left: 5px solid #3f51b5 !important">
+  <ul class="w3-ul w3-margin-4 w3-border" style="width:40%">
+    <li class="w3-hover-blue"><a href="#crear" class="w3-text-dark-grey" style="text-decoration: none">¿Cómo crear un ticket?</a></li>
+    <li class="w3-hover-blue"><a href="#consultar" class="w3-text-dark-grey" style="text-decoration: none">¿Cómo consultar un ticket?</a></li>
+  </ul>
+  <h1 class="w3-center w3-text-indigo animated pulse">Mesa de ayuda</h1>
   <p>
-    La Mesa de ayuda es una plataforma web desarollada por los integrantes del departamento de sistemas de Lagobo Distribuciones S.A
+    La Mesa de Ayuda es una plataforma web desarollada por los integrantes del departamento de sistemas de Lagobo Distribuciones S.A
     con el fin de mantener  un control sobre todos los incidentes relacionados con el área de sistemas de su desarrollo
     y posterior solución.<br/><br/>
 
@@ -14,6 +18,80 @@
     manteniendo un registro completo de todo el proceso de resolución de problemas aumentando así la calidad del servicio y obligando
     a la respuesta más rápida ademas de la mejor posible ya que esta estará sujeta a posterior revisión y verificación por parte
     de quien corresponda.
-  </p>
-</div>
+  </p><br/><br/><br/>
+  <article id="crear" class="w3-card-2 w3-padding" style="width: 90%; margin: auto">
+    <h3 class="w3-center w3-text-indigo">¿Cómo crear un ticket?</h3>
+    <ul style="list-style-type: decimal">
+      <li>
+        Ir al menú en la parte superior izquierda y hacer click sobre el icono de menú.
+        <div class="w3-center">
+          <img src="<?php echo base_url('assets/img/tuto-1.png'); ?>"  />
+        </div>
+      </li>
+      <li>
+        Se despliega un menú de opciones en donde se elige la opción que se desea, en este caso la opció de crear un ticket.
+        <div class="w3-center">
+          <img src="<?php echo base_url('assets/img/tuto-2.png'); ?>"  />
+        </div>
+      </li>
+      <li>
+        Se abre una ventana con un formulario, se debe diligenciar debidamente lo más claro y explicito posible.
+      </li>
+      <li>
+        Posteriormente se debe llenar correctamente el campo de código de seguridad y darle click al botón crear.
+        <div class="w3-center">
+          <img src="<?php echo base_url('assets/img/tuto-3.png'); ?>"  />
+        </div>
+      </li>
+      <li>
+        Si se ha diligenciado correctamente el formulario y no hay ningún problema debería aparecer un mensaje como
+        el siguiente indicando que el ticket ha sido generado satisfactoriamente ademas se mostrará un número con el cual
+        se podrá hacer el seguimiento al ticket o solicitud.
+        <div class="w3-center">
+          <img src="<?php echo base_url('assets/img/tuto-4.png'); ?>"  />
+        </div>
+      </li>
 
+      <li>
+        Tambien la plataforma enviará un correo electrónico mostrando el código del ticket y el link donde podra ver el estado en
+        que se encuentra.
+        <div class="w3-center">
+          <img src="<?php echo base_url('assets/img/tuto-5.png'); ?>"  />
+        </div>
+      </li>
+    </ul>
+  </article>
+  <br/><br/><br/>
+  <article id="consultar" class="w3-card-2 w3-padding" style="width: 90%; margin: auto">
+    <h3 class="w3-center w3-text-indigo">¿Cómo consultar un ticket?</h3>
+    <ul style="list-style-type: decimal">
+      <li>
+        Teniendo el código del ticket ir al menú de la plataforma de Mesa de Ayuda ubicado en la parte superior izquierda.
+        <div class="w3-center">
+          <img src="<?php echo base_url('assets/img/tuto-1.png'); ?>"  />
+        </div>
+      </li>
+      <li>
+        Elegir la opción de Consultar Ticket.
+        <div class="w3-center">
+          <img src="<?php echo base_url('assets/img/tuto-6.png'); ?>"  />
+        </div>
+      </li>
+
+      <li>
+        Se mostrará un formulario en donde se ingresa el codigo del ticket y hecho esto dar click en el boton de consultar
+        <div class="w3-center">
+          <img src="<?php echo base_url('assets/img/tuto-7.png'); ?>"  />
+        </div>
+      </li>
+
+      <li>
+        Se mostrara una seccion en donde podra ver todo lo relacionado con el ticket que usted ha generado ádemas permitirá
+        añadir comentarios a su ticket.
+        <div class="w3-center" >
+          <img src="<?php echo base_url('assets/img/tuto-8.png'); ?>" style="max-width: 85%;" />
+        </div>
+      </li>
+    </ul>
+  </article>
+</div>
--- application/views/ticket/consulta.php
+++ application/views/ticket/consulta.php
@@ -1,4 +1,4 @@
-<div class="w3-center">
+<div class="w3-center w3-margin-8">
    <img src="<?php echo base_url('assets/img/search-icon.png') ?>"></img>
 </div>
 <?php
@@ -6,7 +6,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 
-echo form_open("ticket/consultar", ["class"=>"w3-form w3-center"]);
+echo form_open("ticket/consultar", ["class"=>"w3-form w3-center w3-card-2 animated rotateIn", "style"=>"width: 80%; margin: auto"]);
 
 echo form_label("Código de ticket:<br/>", "ticket-id_ticket", ["class"=>"w3-label"]);
 echo form_input([
@@ -20,4 +20,3 @@ echo form_input([
 echo form_submit("consulta", "consultar", "class='w3-btn w3-indigo w3-margin-8'");
 
 echo form_close();
-
